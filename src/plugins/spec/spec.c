84cdb3606d3e4ae6e720277e3a83e1fe5b34c602
T B <e01306821@student.tuwien.ac.at>
2019-03-27 16:35:58 +0100

    Found error in code...

--- src/plugins/spec/spec.c
+++ src/plugins/spec/spec.c
@@ -893,6 +893,7 @@ int elektraSpecGet (Plugin * handle, KeySet * returned, Key * parentKey)
 	ksRewind (specKS);
 	int ret = doGlobbing (parentKey, ks, specKS, ch, GET, clean);
 	ksAppend (returned, specKS);
+	//Error happens here:
 	ksAppend (returned, ks);
 	ksDel (ks);
 	ksDel (specKS);
